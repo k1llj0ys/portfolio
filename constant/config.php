46076a8cd951d3e9c185beb7ffdfca9629845911
k1llj0ys <132785472+k1llj0ys@users.noreply.github.com>
2024-03-23 14:30:08 +0800

    Add files via upload

---
+++ constant/config.php
@@ -0,0 +1,15 @@
+<?php
+    ob_start();
+    //Start session
+    session_start();
+// Create Contants to store Non Repeating Values
+define('SITEURL', 'http://localhost/portfolio/');
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'portfolio');
+
+
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(msqli_error()); //database connection
+$db_select = mysqli_select_db($conn, DB_NAME) or die(msqli_error()); //selecting database
+?>
